8a0a8bb76e794cbd466c17157d67aac67e4fb506
Clodagh Campbell <campbellc@macstudent.com.au>
2024-12-04 11:35:34 +1100

    counting characters output

---
+++ countingchars.py
@@ -0,0 +1,3 @@
+string = input("what is the input string? ")
+length = len(string)
+print(f"{string} has {length} characters")
--- sayinghello.py
+++ sayinghello.py
@@ -1,2 +1,2 @@
 name = input("what is your name? ")
-print("hello", name)
+print(f"hello {name}")
